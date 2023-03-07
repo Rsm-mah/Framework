3da6c9a64524a5695ee3e38fba0d7d453009827a
Rsm-mah <mahrasamisoa@gmail.com>
2023-03-07 10:23:07 +0100

    Sprint2

--- FrontServlet.java
+++ FrontServlet.java
@@ -1,11 +1,16 @@
 package etu1877.framework.servlet;
 
 import java.io.*;
+import java.util.HashMap;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import etu1877.framework.Mapping;
+
 
 public class FrontServlet extends HttpServlet {
+
+    HashMap<String,Mapping>  MappingUrls;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
